1bc424f4c2dda6660b88022203756b356a873f7f
blitz-flex <abashidzeikaa@gmail.com>
2025-10-19 09:50:14 +0400

    add database models and SQLAlchemy integration registration and program enrollment

--- app.py
+++ app.py
@@ -2,21 +2,57 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from forms import ProgramRegistrationForm, SimpleAccountForm
 import os
 from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 app.secret_key = '25848482828rfrg5rf5'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness_club.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
+db = SQLAlchemy(app)
 
-# --- # --- Helper function to display form errors --- ---
+### --- Model ---###
+
+class ProgramRegistration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    program = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f'ProgramRegistration({self.name}, {self.program})'
+
+
+class UserAccount(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    birthdate = db.Column(db.Date, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    profile_image = db.Column(db.String(100), nullable=True)
+
+    def __repr__(self):
+        return f'UserAccount({self.name}, {self.username})'
+
+def init_db():
+    with app.app_context():
+        if not os.path.exists('fitness_club.db'):
+            db.create_all()
+            print("Created database and tables.")
+
+def create_upload_folder():
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
 def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
             flash(f"{getattr(form, field).label.text}: {error}", 'danger')
-
 
 @app.route("/")
 def index():
@@ -66,9 +102,11 @@ def index():
     ]
     return render_template("index.html", programs=programs)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/faq")
 def faq():
@@ -82,35 +120,73 @@ def form():
     active_form_id = None
 
     if request.method == 'POST':
-        form_type_submitted = request.form.get('form_type')
+        form_type = request.form.get('form_type')
 
-        if form_type_submitted == 'program':
+        # Program registration form
+        if form_type == 'program':
             if program_form.validate_on_submit():
-                #  Data processing
-                print(f"პროგრამა: {program_form.name.data}, {program_form.program.data}, {program_form.phone.data}")
+                registration = ProgramRegistration(
+                    name=program_form.name.data,
+                    program=program_form.program.data,
+                    phone=program_form.phone.data
+                )
+                db.session.add(registration)
+                db.session.commit()
+
                 flash('პროგრამაზე რეგისტრაცია წარმატებით დასრულდა!', 'success')
                 return redirect(url_for('form'))
             else:
                 active_form_id = 'programForm'
                 flash_errors(program_form)
 
-        elif form_type_submitted == 'simple':
+        # user account form
+        elif form_type == 'simple':
             if simple_form.validate_on_submit():
+                # check if user already exists
+                existing_user = UserAccount.query.filter_by(
+                    username=simple_form.username.data
+                ).first()
+
+                if existing_user:
+                    flash('მომხმარებელი უკვე არსებობს!', 'danger')
+                    active_form_id = 'simpleForm'
+                    return render_template("form.html",
+                                           program_form=program_form,
+                                           simple_form=simple_form,
+                                           active_form_id=active_form_id)
+
                 filename = None
                 if simple_form.profile_image.data:
-                    profile_image_file = simple_form.profile_image.data
-                    filename = secure_filename(profile_image_file.filename)
+                    file = simple_form.profile_image.data
+                    filename = secure_filename(file.filename)
                     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                    profile_image_file.save(filepath)
+                    file.save(filepath)
 
-                print(f"ანგარიში: {simple_form.name.data}, {simple_form.username.data}, სურათი: {filename}")
+
+                user = UserAccount(
+                    name=simple_form.name.data,
+                    username=simple_form.username.data,
+                    password=simple_form.password.data,
+                    birthdate=simple_form.birthdate.data,
+                    gender=simple_form.gender.data,
+                    profile_image=filename
+                )
+                db.session.add(user)
+                db.session.commit()
+
                 flash('ანგარიშის შექმნა წარმატებით დასრულდა!', 'success')
                 return redirect(url_for('form'))
             else:
                 active_form_id = 'simpleForm'
                 flash_errors(simple_form)
 
-    return render_template("form.html", program_form=program_form, simple_form=simple_form, active_form_id=active_form_id)
+    return render_template("form.html",
+                           program_form=program_form,
+                           simple_form=simple_form,
+                           active_form_id=active_form_id)
+
 
 if __name__ == '__main__':
+    init_db()
+    create_upload_folder()
     app.run(debug=True)
